fc0e8956cc1518a8ec729595fcab19acdd2adce9
Diego Molina <dimolinas@unal.edu.co>
2024-08-27 18:03:11 -0500

    Initial Commit

---
+++ RamdonQuoateAndImages/RamdonQuoateAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamdonQuoateAndImagesApp.swift
+//  RamdonQuoateAndImages
+//
+//  Created by Diegoe012 on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RamdonQuoateAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
